7be0d412fea3019d991daa27520b7c3cb8590e58
Akshay M Chavan <akshay_chavan2003@yahoo.com>
2020-12-20 23:42:10 +0530

    Add project files.

---
+++ DownloadManager/DownloaderLogic.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DownloadManager
+{
+    class DownloaderLogic
+    {
+        public string DownloadLink { get; set; }
+        public string DownloadDestination { get; set; }
+
+        internal void GetUserInput()
+        {
+            Console.Write("Enter the download URL: ");
+            DownloadLink = Console.ReadLine();
+            Console.Write("Enter the download destination: ");
+            DownloadDestination = Console.ReadLine();
+        }
+
+        internal void Download()
+        {
+            WebClient webClient = new WebClient();
+            webClient.DownloadFile(DownloadLink, DownloadDestination);
+            Console.WriteLine($"The file has been downloaded to {DownloadDestination}");
+        }
+    }
+}
