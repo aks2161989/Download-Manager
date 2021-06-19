56bebbb244535a8c6dfa0b80fe4a3502ebad56a6
Akshay M Chavan <akshay_chavan2003@yahoo.com>
2021-06-19 18:31:21 +0530

    Trying new approach

--- DownloadManager/DownloaderLogic.cs
+++ DownloadManager/DownloaderLogic.cs
@@ -8,91 +8,32 @@ using System.IO;
 
 namespace DownloadManager
 {
-    public class DownloaderLogic
-    {
-        private volatile bool _allowedToRun; // volatile keyword indicates that this field may be modified by multiple threads that are executing at the same time
-
-        private string _source;
-        private string _destination;
-        private int _chunkSize;
-
-        private Lazy<int> _contentLength; // Lazy<int> defers the initialization of _contentLength until it is first used
-
-        public int BytesWritten { get; private set; }
-        public int ContentLength { get { return _contentLength.Value; } }
-
-        public bool Done { get { return ContentLength == BytesWritten; } }
-
-        public DownloaderLogic(string source, string destination, int chunkSize)
-        {
-            _allowedToRun = true;
-
-            _source = source;
-            _destination = destination;
-            _chunkSize = chunkSize;
-            _contentLength = new Lazy<int>(() => Convert.ToInt32(GetContentLength()));
-
-            BytesWritten = 0;
-        }
-
-        private long GetContentLength()
-        {
-            var request = (HttpWebRequest)WebRequest.Create(_source); // Initializes a new WebRequest for a specified URI scheme
-
-            request.Method = "HEAD";
-            /* HTTP 'HEAD' method requests headers that would be returned if the 'HEAD' request's URL was instead requested with the HTTP 'GET' method. For instance, if the URL might produce a large download, a HEAD request could read its Content-Length header to check the filesize without actually downloading the file
-		*/
-
-            using(var response = request.GetResponse())// Returns a response from an internet request
-            {
-                return response.ContentLength; // When overriden in a descendant class, gets or sets the content length of data being received 
-            }
-        }
-        private async Task Start(int range)
-        {
-            if (!_allowedToRun)
-                throw new InvalidOperationException();
-
-            var request = (HttpWebRequest)WebRequest.Create(_source);// Create method initializes a new WebRequest, which makes a request to a URI
-
-            request.Method = "GET";// Method is a property of HttpWebRequest. GET is an HTTP 1.1 protocol verb which means retrieve whatever information is identified by the Request-URI
-
-            request.UserAgent = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;) ";
-            /* UserAgent is a property of HttpWebRequest which gets or sets the value of the User-agent HTTP header. The User-agent HTTP header is a characteristic string that lets servers and network peers identify the application, operating system, vendor and/or version of the requesting user agent. A user agent is a computer program representing a person, for example a browser in a web content. */
-
-            request.AddRange(range);
-            // AddRange method adds a range header to the request. The Range header on a request allows a client to request that it only wants to receive some part of the specified range of bytes in an HTTP entity.
-
-            using (var response = await request.GetResponseAsync())// GetResponseAsync method, when overridden in a descendant class, returns a response to an Internet request as an asynchronous operation
-            {
-                using (var responseStream = response.GetResponseStream())// GetResponseStream method, when overridden in a descendant class, returns the data stream from the Internet resource
-                {
-                    using (var fs = new FileStream(_destination, FileMode.Append, FileAccess.Write, FileShare.ReadWrite))// Initializes a new instance of the FileStream class with the specified path, creation mode, read/write permission and share permission
-                    {
-                        while (_allowedToRun)
-                        {
-                            var buffer = new byte[_chunkSize];
-                            var bytesRead = await responseStream.ReadAsync(buffer, 0, buffer.Length);
-
-                            if (bytesRead == 0) break;
-
-                            await fs.WriteAsync(buffer, 0, bytesRead);
-                            BytesWritten += bytesRead;
-                        }
-                        await fs.FlushAsync();
-                    }
-                }
-            }
-
-        }
-        public Task Start()
-        {
-            _allowedToRun = true;
-            return Start(BytesWritten);
-        }
-        public void Pause()
-        {
-            _allowedToRun = false;
-        }
-    }
+	public class DownloaderLogic
+	{
+		public void Download()
+		{
+			DateTime startTime = DateTime.UtcNow;
+			WebRequest request = WebRequest.Create("https://wds.gcdn.co/wgc/releases_tTrHgLCKHBRiaL/wgc_21.03.00.5224_na/world_of_warships_ww_install_na.exe?enctid=cc7lgfnncntt");
+			WebResponse response = request.GetResponse();
+			using (Stream responseStream = response.GetResponseStream())
+			{
+				using (Stream fileStream = File.OpenWrite(@"f:\\my_downloads\\world_of_warships_ww_install_na.exe"))
+				{
+					byte[] buffer = new byte[4096];
+					int bytesRead = responseStream.Read(buffer, 0, 4096);
+					while (bytesRead > 0)
+					{
+						fileStream.Write(buffer, 0, bytesRead);
+						DateTime nowTime = DateTime.UtcNow;
+						if ((nowTime - startTime).TotalMinutes > 5)
+						{
+							throw new ApplicationException(
+								"Download timed out");
+						}
+						bytesRead = responseStream.Read(buffer, 0, 4096);
+					}
+				}
+			}
+		}
+	}
 }
--- DownloadManager/Program.cs
+++ DownloadManager/Program.cs
@@ -19,30 +19,10 @@ namespace DownloadManager
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
 
-            var fw = new DownloaderLogic("http://download.microsoft.com/download/E/E/2/EE2D29A1-2D5C-463C-B7F1-40E4170F5E2C/KinectSDK-v1.0-Setup.exe", @"F:\KinetSDK.exe", 5120);
-
-            // Display progress...
-            Task.Factory.StartNew(() =>
-            {
-                while (!fw.Done)
-                {
-                    Console.SetCursorPosition(0, Console.CursorTop);
-                    Console.Write(string.Format("ContentLength: {0} | BytesWritten: {1}", fw.ContentLength, fw.BytesWritten));
-                }
-            });
-
-            // Start the download...
-            fw.Start();
-
-            // Simulate pause...
-            Thread.Sleep(500);
-            fw.Pause();
-            Thread.Sleep(2000);
-
-            // Start the download from where we left, and when done print to console.
-            fw.Start().ContinueWith(t => Console.WriteLine("Done"));
-
-            Console.ReadKey();
+            DownloaderLogic dl = new DownloaderLogic();
+            Console.WriteLine("Beginning download...");
+            dl.Download();
+            Console.WriteLine("Download completed.");
         }
     }
 }
