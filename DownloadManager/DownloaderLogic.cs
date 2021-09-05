1098323d2a454694a46f9ff29827c9097b822100
Akshay M Chavan <akshay_chavan2003@yahoo.com>
2021-09-05 18:59:10 +0530

    Added code for resuming downloads

--- DownloadManager/DownloaderLogic.cs
+++ DownloadManager/DownloaderLogic.cs
@@ -12,6 +12,8 @@ namespace DownloadManager
 	{
 		public bool IsPaused { get; set; }
 		public bool IsDownloading { get; set; }
+		string downloadPath = "https://ia803007.us.archive.org/35/items/traque/traque.zip";
+		string filePath = "f:\\my_downloads\\cernaja-Metka.zip";
 		public DownloaderLogic()
 		{
 			IsPaused = false;
@@ -21,11 +23,15 @@ namespace DownloadManager
 		{
 			IsDownloading = true;
 			DateTime startTime = DateTime.UtcNow;
-			WebRequest request = WebRequest.Create("https://dl.myabandonware.com/t/SCW4tNnUIXwjWowiRg8FXwu4Y3cXYAOB7E0o3ZznV0xKqWjztV/Man-of-War-II-Chains-of-Command_Win_EN_ISO-Version.zip");
+			WebRequest request = WebRequest.Create(downloadPath);
+			if (ResumeDownloadIfExists(request, filePath) == true)
+			{
+				return;
+			}
 			WebResponse response = request.GetResponse();
 			using (Stream responseStream = response.GetResponseStream())
 			{
-				using (Stream fileStream = File.OpenWrite(@"f:\\my_downloads\\Man-of-War-II-Chains-of-Command_Win_EN_ISO-Version.zip"))
+				using (Stream fileStream = File.OpenWrite(filePath))
 				{
 					byte[] buffer = new byte[4096];
 					int bytesRead = responseStream.Read(buffer, 0, 4096);
@@ -44,6 +50,80 @@ namespace DownloadManager
 				}
 			}
 		}
+
+		private bool ResumeDownloadIfExists(WebRequest webRequest, string v)
+		{
+			if (File.Exists(v))
+			{
+				webRequest.Method = "HEAD";
+				long downloadFileSize = -1;
+				using (WebResponse webResponse = webRequest.GetResponse())
+				{
+					if (long.TryParse(webResponse.Headers.Get("Content-Length"), out long contentLength))
+					{
+						downloadFileSize = contentLength; //Gets the size of the ownload in bytes
+					}
+				}
+
+				FileInfo fileInfo = new FileInfo(v);
+				long existingFileSize = fileInfo.Length; // The size of the existing file in bytes
+				if (downloadFileSize <= existingFileSize)
+				{
+					return true; // If existing file is of the same size as the download or of larger size, no need to download
+				}
+
+				// File exists but is smaller than download, so we can resume download
+				DateTime startTime = DateTime.UtcNow;
+				WebRequest request = WebRequest.Create(downloadPath);
+				WebResponse response = request.GetResponse();
+				using (Stream responseStream = response.GetResponseStream())
+				{
+                    byte[] bufferDownload = new byte[4096];
+                    int bytesReadFromDownload = 0;
+                    byte[] bufferFile = new byte[4096];
+                    int bytesReadFromFile = 0;
+                    using (Stream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.ReadWrite))
+					{
+						
+						 bytesReadFromDownload = responseStream.Read(bufferDownload, 0, 4096);
+
+						
+						bytesReadFromFile = fileStream.Read(bufferFile, 0, 4096);
+						while (bytesReadFromDownload > 0 && IsPaused == false)
+						{
+							IsDownloading = true;
+							if (bytesReadFromFile > 0 && bufferDownload.SequenceEqual(bufferFile))
+							{
+								bytesReadFromDownload = responseStream.Read(bufferDownload, 0, 4096);
+								bytesReadFromFile = fileStream.Read(bufferFile, 0, 4096);
+								continue;
+							}
+							else if (bytesReadFromFile == 0)
+							{
+								using (var stream = new FileStream(filePath, FileMode.Append))
+								{
+									DateTime nowTime = DateTime.UtcNow;
+									if ((nowTime - startTime).TotalMinutes > 5)
+									{
+										throw new ApplicationException(
+									"Download timed out");
+									}
+									stream.Write(bufferDownload, 0, bytesReadFromDownload);
+								}
+								bytesReadFromDownload = responseStream.Read(bufferDownload, 0, 4096);
+							}
+						}
+						IsDownloading = false;
+					}
+				}
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
 		public void Pause()
 		{
 			char? input = null;
